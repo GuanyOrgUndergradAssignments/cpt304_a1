87370964db9dadc2b88720a96ffe670f61b2b4bd
brosnanliew <121977414+brosnanliew@users.noreply.github.com>
2024-04-04 15:21:31 +0800

    fixed and added unit testing

--- src/edu/a1/tests/TestQueryBookCmd.java
+++ src/edu/a1/tests/TestQueryBookCmd.java
@@ -2,7 +2,6 @@ package edu.a1.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,12 +10,9 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 import edu.a1.book.Book;
-import edu.a1.system.IOInteraction;
 import edu.a1.system.LibrarySystem;
-import edu.a1.system.User;
 import edu.a1.system.cmd.QueryBookCommand;
 import edu.a1.database.*;
 
@@ -32,6 +28,16 @@ public class TestQueryBookCmd {
         LibrarySystem.initLibrarySystem(new IOInteractionForTests(inputBuffer));
         bookDB = LibrarySystem.bookStorage;
 
+    }
+
+    @AfterEach
+    public void clearDataBase() {
+        List<Book> stashedBooks = new ArrayList<>(bookDB.findAll());
+
+        // clear the databases
+        for(var b : stashedBooks) {
+            bookDB.delete(b);
+        }
     }
 
     private static void inputCommand(String cmd) {
@@ -61,86 +67,155 @@ public class TestQueryBookCmd {
         Book book = new Book("MeowMeow", "1234567890123", 5, "Fantasy", "2022", "Cat", 19.99);
         bookDB.save(book);
 
-        // Set up the QueryBookCommand
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--ISBN", "1234567890123"));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
 
-        // assertTrue(inputBuffer.toString().contains("Book found:"));
-        assertTrue(inputBuffer.toString().contains(book.toString()));
+        // Set up the QueryBookCommand
+        inputCommand("query-book --ISBN 1234567890123");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains("Book found:"));
+        assertTrue(out.contains(book.toString()));
+        
+        outBuffer.delete(0, outBuffer.length());
+
     }
 
     @Test
     public void testHandleAuthorBookNameQuery() {
         Book book = new Book("MeowMeow", "1234567890123", 5, "Fantasy", "2022", "Cat", 19.99);
         bookDB.save(book);
-        
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--author-and-book-name", "Cat", "MeowMeow"));
 
-        assertTrue(inputBuffer.toString().contains("Book found:"));
-        assertTrue(inputBuffer.toString().contains(book.toString()));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
+
+        // Set up the QueryBookCommand
+        inputCommand("query-book --author-and-book-name Cat MeowMeow");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains("Book found:"));
+        assertTrue(out.contains(book.toString()));
         
+        outBuffer.delete(0, outBuffer.length());
     }
     
     @Test
     public void testHandleCategoryQuery() {
         Book book = new Book("MeowMeow", "1234567890123", 5, "Fantasy", "2022", "Cat", 19.99);
         bookDB.save(book);
-        
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--category", "Fantasy"));
 
-        assertTrue(inputBuffer.toString().contains("Book found:"));
-        assertTrue(inputBuffer.toString().contains(book.toString()));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
+
+        // Set up the QueryBookCommand
+        inputCommand("query-book --category Fantasy");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains(book.toString()));
+        
+        outBuffer.delete(0, outBuffer.length());
         
     }
 
     @Test
     public void testHandleAllQuery() {
-        Book book = new Book("MeowMeow", "1234567890123", 5, "Fantasy", "2022", "Cat", 19.99);
-        bookDB.save(book);
-        
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--all"));
+        List<Book> testBooks = new ArrayList<>();
+        testBooks.add(new Book("MeowMeow", "1234567890123", 5, "Fantasy", "2022", "Cat", 19.99));
+        testBooks.add(new Book("MeowMeow2", "1234567890124", 5, "Fantasy", "2022", "Cat", 19.99));
 
-        assertTrue(inputBuffer.toString().contains("Book found:"));
-        assertTrue(inputBuffer.toString().contains(book.toString()));
-        
+        for (Book book : testBooks) {
+            bookDB.save(book);
+        }
+
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
+
+        // Set up the QueryBookCommand
+        inputCommand("query-book --all ");
+        var out = outBuffer.toString();
+
+        // Assert that the expected output is present in the output buffer
+        assertTrue(out.contains("A total of " + testBooks.size() + " books found."));
+        for (int i = 0; i < testBooks.size(); ++i) {
+            assertTrue(out.contains("Book " + i + ":"));
+            assertTrue(out.contains(testBooks.get(i).toString()));
+        }
+
+        // Clear the output buffer after running the test
+        outBuffer.delete(0, outBuffer.length());        
     }
 
     @Test
     public void testHandleNoBookFound() {
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--ISBN", "1234567890123"));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
 
-        assertTrue(inputBuffer.toString().contains("No book found."));
-        
+        // Set up the QueryBookCommand
+        inputCommand("query-book --ISBN 1234567890123");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains("No book has the ISBN."));
+
+        outBuffer.delete(0, outBuffer.length());
     }
 
     @Test
     public void testHandleNoBookCategory() {
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--category", "Fantasy"));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
 
-        assertTrue(inputBuffer.toString().contains("No book has the category."));
+        // Set up the QueryBookCommand
+        inputCommand("query-book --category Fantasy");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains("No book has the category."));
+        
+        outBuffer.delete(0, outBuffer.length());
         
     }
 
     @Test
     public void testHandleNoBooksExist() {
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--all"));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
 
-        assertTrue(inputBuffer.toString().contains("No books exist."));
+        // Set up the QueryBookCommand
+        inputCommand("query-book --all ");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains("No books exist."));
+        
+        outBuffer.delete(0, outBuffer.length());
         
     }
 
     @Test
     public void testHandleNoBookAuthorAndBookName() {
-        QueryBookCommand queryBookCommand = new QueryBookCommand();
-        queryBookCommand.handle(Arrays.asList("--author-and-book-name", "Cat", "MeowMeow"));
+        // first drain the command output buffer
+        var outBuffer = ((IOInteractionForTests)LibrarySystem.getIO()).myOwnedBuffer;
+        outBuffer.delete(0, outBuffer.length());
 
-        assertTrue(inputBuffer.toString().contains("No book has the author and bookname."));
+        // Set up the QueryBookCommand
+        inputCommand("query-book --author-and-book-name Cat MeowMeow");
+
+        var out = outBuffer.toString();
+
+        assertTrue(out.contains("No book has the author and bookname."));
+        
+        outBuffer.delete(0, outBuffer.length());
         
     }
 
@@ -154,6 +229,7 @@ public class TestQueryBookCmd {
         // Test with an invalid number of arguments for "--author-and-book-name"
         assertThrows(IllegalArgumentException.class, () -> queryBookCommand.handle(Arrays.asList("--author-and-book-name", "author")));
 
-        // Add more test cases for other options if needed
+        // Test with an invalid number of arguments for "--category"
+        assertThrows(IllegalArgumentException.class, () -> queryBookCommand.handle(Collections.singletonList("--category")));
     }
 }
