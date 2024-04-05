57f11434f11acd4de377d2b51b3ca23302462117
Hanyu Zhang <safra_zhang@sina.com>
2024-04-05 11:06:33 +0800

    Test Exit Login and Logout

---
+++ src/edu/a1/tests/TestLoginCmd.java
@@ -0,0 +1,61 @@
+package edu.a1.tests;
+
+import edu.a1.database.UserManager;
+import edu.a1.system.IOInteraction;
+import edu.a1.system.LibrarySystem;
+import edu.a1.system.User;
+import edu.a1.system.auth.SystemAuthenticator;
+import edu.a1.system.cmd.LoginCommand;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class TestLoginCmd {
+
+    private static StringBuilder inputBuffer;
+    private static UserManager usrDB;
+
+
+    @Test
+    public void testHandleWithValidArguments() {
+        inputBuffer = new StringBuilder("");
+        LibrarySystem.initLibrarySystem(new IOInteractionForTests(inputBuffer));
+        LibrarySystem.authenticator = new SystemAuthenticator();
+
+        usrDB = LibrarySystem.userStorage;
+        usrDB.save(new User("username", "password"));
+
+        LoginCommand loginCommand = new LoginCommand();
+
+        List<String> validArguments = Arrays.asList("username", "password");
+        assertDoesNotThrow(() -> loginCommand.handle(validArguments));
+    }
+
+    @Test
+    public void testHandleWithInvalidArguments() {
+        LoginCommand loginCommand = new LoginCommand();
+
+        List<String> invalidArguments = Collections.singletonList("username");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> loginCommand.handle(invalidArguments));
+        assertEquals("the number of arguments must be 2.", exception.getMessage());
+    }
+
+    @Test
+    public void testHelpMessage() {
+        LoginCommand loginCommand = new LoginCommand();
+
+        String expectedHelpMessage = "Usage: Usage: login USERNAME PASSWORD\n" +
+                "Effect: log in the user with USERNAME\n" +
+                "Options: \n" +
+                "\t --help: displays the help message.\n" +
+                "Parameters: \n" +
+                "\t USERNAME: the username of the user.\n" +
+                "\t PASSWORD: the password of the user.\n";
+        assertEquals(expectedHelpMessage, loginCommand.helpMessage());
+    }
+
+}
---
+++ src/edu/a1/tests/TestLogoutCmd.java
@@ -0,0 +1,51 @@
+package edu.a1.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import edu.a1.database.UserManager;
+import edu.a1.system.LibrarySystem;
+import edu.a1.system.User;
+import edu.a1.system.auth.SystemAuthenticator;
+import edu.a1.system.cmd.LoginCommand;
+import org.junit.Test;
+
+import edu.a1.system.cmd.LogoutCommand;
+
+public class TestLogoutCmd {
+
+    private static StringBuilder inputBuffer;
+    private static UserManager usrDB;
+
+    @Test
+    public void testHandle() {
+        inputBuffer = new StringBuilder("");
+        LibrarySystem.initLibrarySystem(new IOInteractionForTests(inputBuffer));
+        LibrarySystem.authenticator = new SystemAuthenticator();
+
+        usrDB = LibrarySystem.userStorage;
+        usrDB.save(new User("username", "password"));
+        LoginCommand loginCommand = new LoginCommand();
+
+        List<String> validArguments = Arrays.asList("username", "password");
+        loginCommand.handle(validArguments);
+
+        LogoutCommand logoutCommand = new LogoutCommand();
+
+        assertDoesNotThrow(() -> logoutCommand.handle(Collections.emptyList()));
+    }
+
+    @Test
+    public void testHelpMessage() {
+        LogoutCommand logoutCommand = new LogoutCommand();
+
+        String expectedHelpMessage = "Usage: Usage: logout\n"
+                + "Effect: log out the currently logged in user.\n"
+                + "Options: \n"
+                + "\t --help: displays the help message.\n";
+        assertEquals(expectedHelpMessage, logoutCommand.helpMessage());
+    }
+}
